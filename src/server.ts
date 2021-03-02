abff6b21df7307f4050350fbe9ea52baa39c77f0
wenderson1 <wenderson@alunos.fho.edu.br>
2021-03-02 00:45:59 -0300

    Projeto Criado

--- src/server.ts
+++ src/server.ts
@@ -15,3 +15,7 @@ app.post("/", (request, response) => {
 });
 
 app.listen(3333, () => console.log("Server is running"));
+
+/***
+ * Gastrore é uma api feita em Node onde os usuários poderão cadastrar suas receitas e consultar e interagir com as receitas de outros por Likes/Dislikes ou comentários
+ */
