44c5d98d3c1e585867efac84c57beedc7d18f9d5
wenderson1 <wenderson@alunos.fho.edu.br>
2021-04-05 06:39:01 -0300

    Tava dando ruim em algumas coisas, apaguei e vou começar a autenticação

--- src/controllers/CommentaryController.ts
+++ src/controllers/CommentaryController.ts
@@ -1,34 +1,12 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
-import {CommentaryRepository} from '../repositories/CommentaryRepository'
+import { CommentaryRepository } from '../repositories/CommentaryRepository'
+import { UserRepository } from '../repositories/UsersRepository';
+import { RecipeRepository } from '../repositories/RecipeRepository';
 
 class CommentaryController{
 
-    async create(request: Request, response: Response) {
-        const { comment, user } = request.body;
-
-        const commentaryRepository = getCustomRepository(CommentaryRepository)
-
-        const commentaryAlreadyExists = await commentaryRepository.findOne({
-            user
-        })
-
-        if (commentaryAlreadyExists) {
-            return response.status(400).json(
-                {
-                    error:"This user already comment"
-                }
-            )
-        }
-
-        const commentary = commentaryRepository.create({
-            comment,user
-        });
-        
-        await commentaryRepository.save(commentary);
-
-        return response.json(commentary);
-    }
+  
 }
 
 
--- src/repositories/RecipeRepository.ts
+++ src/repositories/RecipeRepository.ts
@@ -3,7 +3,7 @@ import { EntityRepository, Repository } from "typeorm";
 import { Recipe } from "../models/Recipe";
 
 @EntityRepository(Recipe)
-class CommentaryRepository extends Repository<Recipe>{
+class RecipeRepository extends Repository<Recipe>{
 }
 
-export { CommentaryRepository };
+export { RecipeRepository };
